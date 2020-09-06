a33aa2bba2b39fa19c850a0393dc0b321d06c707
cgriffin332 <cgriffin332@gmail.com>
2020-09-06 13:38:19 -0400

    added quiz questions container and added function so that on click of start button, the title container is hidden and question container is shown.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 // DOM variables
 var timerValue = document.getElementById("timerValue");
 var startButton = document.getElementById("startQuiz");
+var startContainer = document.getElementById("container1");
+var questionContainer = document.getElementById("container2");
 
 // when start button is clicked, timer is set at 75 and begins.
 startButton.addEventListener("click", function(){
     // start timer number
     var time = 76;
+    // hides this container
+    startContainer.style.display = "none"
+    // shows question to begin quiz
+    questionContainer.style.display = "block"
     // start timer
     var setTimer = setInterval(function(){
         if (time > 0) {
@@ -14,7 +20,8 @@ startButton.addEventListener("click", function(){
         } else {
             clearInterval(setTimer);
         }
-    }, 500)
+    }, 1000)
+    
 })
 
 // 
