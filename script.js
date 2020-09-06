392825c4a246a7656afdfd4334a318430f86041f
cgriffin332 <cgriffin332@gmail.com>
2020-09-06 12:40:29 -0400

    added setInterval function to create countdown timer that begins when start button is clicked

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// timer value variable
+// DOM variables
 var timerValue = document.getElementById("timerValue");
+var startButton = document.getElementById("startQuiz");
+
+// when start button is clicked, timer is set at 75 and begins.
+startButton.addEventListener("click", function(){
+    // start timer number
+    var time = 76;
+    // start timer
+    var setTimer = setInterval(function(){
+        if (time > 0) {
+            time--;
+            timerValue.textContent = time;
+        } else {
+            clearInterval(setTimer);
+        }
+    }, 500)
+})
+
+// 
