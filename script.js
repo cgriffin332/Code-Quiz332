f5b285b0b1f0305acb48fb01060abfbab6136009
cgriffin332 <cgriffin332@gmail.com>
2020-09-06 15:44:08 -0400

    created final score container and made it show at the end of the timer.

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ var timerValue = document.getElementById("timerValue");
 var startButton = document.getElementById("startQuiz");
 var startContainer = document.getElementById("container1");
 var questionContainer = document.getElementById("container2");
+var finalScoreContainer = document.getElementById("container3");
 
 // when start button is clicked, timer is set at 75 and begins.
 startButton.addEventListener("click", function(){
     // start timer number
     var time = 76;
     // hides this container
-    startContainer.style.display = "none"
+    startContainer.style.display = "none";
     // shows question to begin quiz
-    questionContainer.style.display = "block"
+    questionContainer.style.display = "block";
     // start timer
     var setTimer = setInterval(function(){
         if (time > 0) {
@@ -19,9 +20,11 @@ startButton.addEventListener("click", function(){
             timerValue.textContent = time;
         } else {
             clearInterval(setTimer);
+            // get final score page to show
+            questionContainer.style.display = "none";
+            finalScoreContainer.style.display = "block";
         }
-    }, 1000)
-    
+    }, 500)
 })
 
 // 
